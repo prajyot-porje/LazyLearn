30445e177fc6d58977caa65ebad7d3f262741a08
prajyot-porje <prajyotyt18@gmail.com>
2025-01-25 23:29:42 +0530

    Update dashboard page styles for improved responsiveness and dark mode support

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='bg-black h-screen w-screen'>
+    <div className='bg-white dark:bg-black h-screen w-screen'>
 
-        <div className="bg-white h-screen mx-28 pt-28 ">
+        <div className=" h-screen lg:mx-28 sm:mx-auto pt-28 ">
             <CardList/>
         </div>
         
